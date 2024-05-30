1d87d5d995ace62835231e13295b42e1002ea463
internet-unable <polocatic@gmail.com>
2024-05-30 17:38:44 +0300

    Create 02-create-users-table.sql
    Example of how to create a table

---
+++ 02-create-users-table.sql
@@ -0,0 +1,14 @@
+-- PostgreSQL syntax
+-- CREATE TYPE employment_status AS ENUM('employed', 'self-employed', 'unemployed');
+-- CREATE TABLE users (
+--     full_name VARCHAR(200),
+--     yearly_salary INT,
+--     current_status employment_status
+-- );
+
+-- MySQL syntax
+-- CREATE TABLE users (
+--     full_name VARCHAR(200),
+--     yearly_salary INT,
+--     current_status ENUM('employed', 'self-employed', 'unemployed') 
+-- );
