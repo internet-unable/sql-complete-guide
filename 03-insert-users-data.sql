2852b47df0e0abb4efbf909fd3dc4fefe593ddba
internet-unable <polocatic@gmail.com>
2024-05-30 18:38:29 +0300

    Create 03-insert-users-data.sql
    Example of how to add data to table

---
+++ 03-insert-users-data.sql
@@ -0,0 +1,11 @@
+INSERT INTO users (full_name, yearly_salary, current_status) VALUES(
+    'Konstantin', 20000, 'self-employed'
+);
+
+INSERT INTO users (full_name, yearly_salary, current_status) VALUES(
+    'Andrew', 0, 'unemployed'
+);
+
+INSERT INTO users (full_name, yearly_salary, current_status) VALUES(
+    'Alex', 10000, 'employed'
+);
