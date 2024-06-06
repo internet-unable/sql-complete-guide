31670c38ac2bdb122feec4697ad082ec6b55ddad
internet-unable <polocatic@gmail.com>
2024-06-06 15:45:14 +0300

    Generated colimns
    Example of how to create generated column, using MySQL

--- 15-generated-columns.sql
+++ 15-generated-columns.sql
@@ -7,6 +7,7 @@ CREATE TABLE users(
     id INT PRIMARY KEY AUTO_INCREMENT,
     first_name VARCHAR(200) NOT NULL,
     last_name VARCHAR(200) NOT NULL,
+    full_name VARCHAR(401) GENERATED ALWAYS AS (CONCAT(first_name,' ',last_name)),
     yearly_salary INT CHECK (yearly_salary > 0),
     current_status ENUM('self-employed', 'employed', 'unemployed')
 );
@@ -24,26 +25,5 @@ CREATE TABLE conversations(
     message TEXT,
     date_sent TIMESTAMP
 );
-
--- PostgreSQL
--- CREATE TYPE employment_status AS ENUM('self-employed', 'employed', 'unemployed'); -- this type was already created before
-CREATE TABLE users(
-    id SERIAL PRIMARY KEY,
-    full_name VARCHAR(300) NOT NULL,
-    yearly_salary INT CHECK (yearly_salary > 0),
-    current_status employment_status
-);
-CREATE TABLE employers(
-    id SERIAL PRIMARY KEY,
-    company_name VARCHAR(300) NOT NULL,
-    company_address VARCHAR(300) NOT NULL,
-    yearly_revenue FLOAT CHECK (yearly_revenue > 0),
-    is_hiring BOOLEAN DEFAULT FALSE
-);
-CREATE TABLE conversations(
-    id SERIAL PRIMARY KEY,
-    user_id INT,
-    employer_id INT,
-    message TEXT,
-    date_sent TIMESTAMP
-);
+INSERT INTO users(first_name, last_name, yearly_salary, current_status)
+VALUES('Alex', 'Stone', 19000, 'self-employed');
