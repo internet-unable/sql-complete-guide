88a1b192b435c243fb117d5ada3686f122dd3951
internet-unable <polocatic@gmail.com>
2024-06-08 14:42:33 +0300

    Update 16-task.sql
    Completed task:
    - Update table and add sensible constaints

--- 16-task.sql
+++ 16-task.sql
@@ -1,18 +1,33 @@
+-- Task #1
+-- Create a new database for the online shop
 -- CREATE DATABASE online_shop;
 
+-- Task #2 and #3
+-- Add a table for products. Choose appropriate column names + data types
 -- CREATE TABLE products(
 --     product_name VARCHAR(200),
 --     product_desc VARCHAR(600),
---     product_price NUMERIC(7, 2),
+--     product_price NUMERIC(7,2),
 --     product_amount_in_stock INTEGER,
 --     product_image VARCHAR(600)
 -- );
 
-INSERT INTO products(product_name, product_desc, product_price, product_amount_in_stock, product_image)
-VALUES(
-    'iPhone 15 Pro Max',
-    'iPhone 15 Pro Max - перший iPhone, виготовлений з титану авіаційного класу, виготовленого з того ж сплаву, який використовують космічні кораблі для польотів на Марс.',
-    1000,
-    100,
-    'some/some/product/image.png'
-)
+-- Task #4
+-- Insert dummy data into created table
+-- INSERT INTO products(product_name, product_desc, product_price, product_amount_in_stock, product_image)
+-- VALUES(
+--     'iPhone 15 Pro Max',
+--     'iPhone 15 Pro Max - перший iPhone, виготовлений з титану авіаційного класу, виготовленого з того ж сплаву, який використовують космічні кораблі для польотів на Марс.',
+--     1000,
+--     100,
+--     'some/some/product/image.png'
+-- );
+
+-- Task #5
+-- Update table and add sensible constraints
+ALTER TABLE products
+MODIFY COLUMN product_name VARCHAR(300) NOT NULL,
+MODIFY COLUMN product_desc VARCHAR(600) NOT NULL,
+MODIFY COLUMN product_price NUMERIC(7,2) CHECK (product_price > 0),
+MODIFY COLUMN product_amount_in_stock INTEGER NOT NULL,
+MODIFY COLUMN product_image VARCHAR(600) NOT NULL;
