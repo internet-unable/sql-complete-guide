6215e007af883ce37399bd4dce671cac6c59cfdf
internet-unable <polocatic@gmail.com>
2024-06-11 17:32:41 +0300

    Update 21-select-data.sql
    Example of some features of data reading

--- 21-select-data.sql
+++ 21-select-data.sql
@@ -2,3 +2,11 @@ SELECT * FROM sales;
 
 SELECT customer_name, product_name, volume, date_created
 FROM sales;
+
+SELECT
+    customer_name,
+    product_name,
+    volume AS total_sales, -- will rename column name from volume to total_sales, only in result set
+    -- volume / 1000 -- alse possible and only for result set
+    date_created
+FROM sales;
