586f79f08269fd4700e6221e2126a1e2a630173f
internet-unable <polocatic@gmail.com>
2024-06-19 17:40:12 +0300

    Update 06-left-join.sql
    Example of how to use multiple LEFT JOIN

--- Data normalization and relationships - split and join data/06-left-join.sql
+++ Data normalization and relationships - split and join data/06-left-join.sql
@@ -1,2 +1,4 @@
 SELECT * FROM addresses
 LEFT JOIN users ON addresses.id = users.address
+-- In case when we have multiple LEFT JOIN, left table will be previous result
+LEFT JOIN city ON city.id = addresses.city_id;
