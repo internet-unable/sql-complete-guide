a42d0ed1c67129c12b66e59f8bb272e1894122cf
internet-unable <polocatic@gmail.com>
2024-06-21 18:42:09 +0300

    Create 09-create-tables-with-references.sql
    Example of how to create tables with relationships, using REFERENCES

---
+++ Data normalization and relationships - split and join data/09-create-tables-with-references.sql
@@ -0,0 +1,50 @@
+-- Drep previous tables
+-- // ----------------------------------------------------------------------------------------------------
+-- DROP TABLE users;
+-- DROP TABLE addresses;
+-- DROP TABLE city;
+
+-- MySQL
+-- // ----------------------------------------------------------------------------------------------------
+CREATE TABLE cities (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(300) NOT NULL
+);
+
+CREATE TABLE addresses (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    street VARCHAR(300) NOT NULL,
+    house_number VARCHAR(50) NOT NULL,
+    city_id INT NOT NULL
+);
+
+CREATE TABLE users (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(300) NOT NULL,
+    last_name VARCHAR(300) NOT NULL,
+    email VARCHAR(300) NOT NULL,
+    address_id INT,
+    FOREIGN KEY (address_id) REFERENCES addresses (id) ON DELETE CASCADE
+);
+
+-- PostgreSQL
+-- // ----------------------------------------------------------------------------------------------------
+CREATE TABLE cities (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(300) NOT NULL
+);
+
+CREATE TABLE addresses (
+    id SERIAL PRIMARY KEY,
+    street VARCHAR(300) NOT NULL,
+    house_number VARCHAR(50) NOT NULL,
+    city_id INT NOT NULL
+);
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(300) NOT NULL,
+    last_name VARCHAR(300) NOT NULL,
+    email VARCHAR(300) NOT NULL,
+    address_id INT REFERENCES addresses(id) ON DELETE CASCADE
+);
