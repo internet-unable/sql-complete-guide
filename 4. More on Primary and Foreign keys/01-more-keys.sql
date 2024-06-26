5ed558941751254883bdee9a2218978828b1f53f
internet-unable <polocatic@gmail.com>
2024-06-26 21:07:37 +0300

    Update 01-more-keys.sql
    Exasmple of how to use composite primary and foreign keys

--- 4. More on Primary and Foreign keys/01-more-keys.sql
+++ 4. More on Primary and Foreign keys/01-more-keys.sql
@@ -1,6 +1,51 @@
+-- DROP TABLE projects_employees;
+-- DROP TABLE intranet_accounts;
+-- DROP TABLE projects;
+-- DROP TABLE employees;
+-- DROP TABLE teams;
+-- DROP TABLE company_buildings;
+
+CREATE TABLE projects (
+    id INT PRIMARY KEY AUTO_INCREMENT, -- MySQL
+    -- id SERIAL PRIMARY KEY, -- PostgreSQL
+    title VARCHAR(300) NOT NULL,
+    deadline DATE
+);
+
+CREATE TABLE company_buildings (
+    id INT PRIMARY KEY AUTO_INCREMENT, -- MySQL
+    -- id SERIAL PRIMARY KEY, -- PostgreSQL
+    name VARCHAR(300) NOT NULL
+);
+
+CREATE TABLE teams (
+    id INT PRIMARY KEY AUTO_INCREMENT, -- MySQL
+    -- id SERIAL PRIMARY KEY, -- PostgreSQL
+    name VARCHAR(300) NOT NULL,
+    building_id INT REFERENCES company_buildings(id) ON DELETE SET NULL
+);
+
+CREATE TABLE employees (
+    id INT PRIMARY KEY AUTO_INCREMENT, -- MySQL
+    -- id SERIAL PRIMARY KEY, -- PostgreSQL
+    first_name VARCHAR(300) NOT NULL,
+    last_name VARCHAR(300) NOT NULL,
+    birthdate DATE NOT NULL,
+    email VARCHAR(300) NOT NULL,
+    team_id INT REFERENCES teams(id) ON DELETE SET NULL
+);
+
+CREATE TABLE intranet_accounts (
+    id INT PRIMARY KEY AUTO_INCREMENT, -- MySQL
+    -- id SERIAL PRIMARY KEY, -- PostgreSQL
+    email VARCHAR(300) REFERENCES employees(email) ON DELETE CASCADE,
+    password VARCHAR(300) NOT NULL
+);
+
 -- Intermediate table => n:n relation
 CREATE TABLE projects_employees(
-    employee_id INT REFERENCES employees(id) ON DELETE CASCADE,
+    employee_id INT,
     project_id INT REFERENCES projects(id) ON DELETE CASCADE,
-    PRIMARY KEY (employee_id, project_id)
+    PRIMARY KEY (employee_id, project_id),
+    FOREIGN KEY (employee_id) REFERENCES employees ON DELETE CASCADE
 );
