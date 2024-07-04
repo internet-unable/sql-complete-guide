ec6ed0113382c9f2e8a1b1edf179e40e35724952
internet-unable <polocatic@gmail.com>
2024-07-04 15:52:32 +0300

    Update 03-create-index.sql
    Extra example on how to create INDEX, during table creation

--- 8. Using Indexes/03-create-index.sql
+++ 8. Using Indexes/03-create-index.sql
@@ -1,1 +1,11 @@
 CREATE INDEX salary_index ON users(salary);
+
+CREATE TABLE users (
+    id INT PRIMARY KEY AUTO_INCREMENT, -- MySQL
+    --   id SERIAL PRIMARY KEY, -- Postgresql
+    first_name VARCHAR(300) NOT NULL,
+    last_name VARCHAR(300) NOT NULL,
+    email VARCHAR(300) UNIQUE NOT NULL,
+    salary INT CHECK (salary > 0),
+    INDEX salary_index(salary) -- another way how to create index, when creating table
+);
